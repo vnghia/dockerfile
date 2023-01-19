6a3540c52d1c34282921e30e9220ca03610b515f
Vo Van Nghia <vanvnghia@gmail.com>
2023-01-20 00:10:29 +0100

    rlang: move install.packages to another file

---
+++ rlang/packages.r
@@ -0,0 +1,13 @@
+options(install.packages.compile.from.source = "never")
+
+# ggplot2
+install.packages(c("ggplot2", "scico"))
+
+# rmarkdown
+install.packages(c("rmarkdown", "bookdown", "tikzDevice", "kableExtra"))
+
+# data
+install.packages(c("dplyr", "reshape2", "tidyverse"))
+
+# python
+install.packages(c("reticulate"))
