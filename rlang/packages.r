8392ceb71058bba98722d826f73ebc14e7814dcf
Vo Van Nghia <vanvnghia@gmail.com>
2023-01-20 00:37:40 +0100

    rlang: force binary installations

--- rlang/packages.r
+++ rlang/packages.r
@@ -1,13 +1,16 @@
 options(install.packages.compile.from.source = "never")
 
 # ggplot2
-install.packages(c("ggplot2", "scico"))
+install.packages(c("ggplot2", "scico"), type = "binary")
 
 # rmarkdown
-install.packages(c("rmarkdown", "bookdown", "tikzDevice", "kableExtra"))
+install.packages(
+  c("rmarkdown", "bookdown", "tikzDevice", "kableExtra"),
+  type = "binary"
+)
 
 # data
-install.packages(c("dplyr", "reshape2", "tidyverse"))
+install.packages(c("dplyr", "reshape2", "tidyverse"), type = "binary")
 
 # python
-install.packages(c("reticulate"))
+install.packages(c("reticulate"), type = "binary")
