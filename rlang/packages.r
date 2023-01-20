f5e5f29b590a288cf27a4cde180e48d83751f56c
Vo Van Nghia <vanvnghia@gmail.com>
2023-01-20 03:32:47 +0100

    rlang: remove rmarkdown (use bookdown version)

--- rlang/packages.r
+++ rlang/packages.r
@@ -2,9 +2,7 @@
 install.packages(c("ggplot2", "scico"))
 
 # rmarkdown
-install.packages(
-  c("rmarkdown", "bookdown", "tikzDevice", "kableExtra")
-)
+install.packages(c("bookdown", "tikzDevice", "kableExtra"))
 
 # data
 install.packages(c("dplyr", "reshape2", "tidyverse"))
