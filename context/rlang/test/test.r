3169ca53c32c2780e11b1431486bf803238db236
Vo Van Nghia <vanvnghia@gmail.com>
2023-01-21 22:12:29 +0100

    add kable

--- context/rlang/test/test.r
+++ context/rlang/test/test.r
@@ -14,3 +14,10 @@ knitr::opts_chunk$set(dev = "tikz", echo = F, cache = T)
 options(tikzDefaultEngine = "luatex")
 
 np <- import("numpy")
+
+## ---- kable ----
+mtcars %>%
+  kbl(
+    booktabs = T, escape = F,
+  ) %>%
+  kable_styling(latex_options = c("hold_position", "scale_down"))
