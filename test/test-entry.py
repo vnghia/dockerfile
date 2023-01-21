9bf0147e1f93c685e9ae0d8f89f317f5e67973e4
Vo Van Nghia <vanvnghia@gmail.com>
2023-01-21 16:21:51 +0100

    github(action): add testdata

--- test/test-entry.py
+++ test/test-entry.py
@@ -1,4 +1,6 @@
 import argparse
+import json
+from pathlib import Path
 
 import docker
 
@@ -8,10 +10,19 @@ def main():
         prog="test-entry",
     )
     parser.add_argument("imageid")
+    parser.add_argument("testdata", type=Path)
     args = parser.parse_args()
 
     client = docker.from_env()
-    print(client.containers.run(args.imageid, "echo Hello World"))
+
+    if args.testdata.exits():
+        with open(args.testdata, "r") as f:
+            testdata = json.load(f)
+            print(
+                client.containers.run(
+                    args.imageid, testdata.pop("command"), **testdata
+                ).decode("utf-8")
+            )
 
 
 if __name__ == "__main__":
