ff5b671445bd0413693ec89b2bfe52e0b965ebe2
Vo Van Nghia <vanvnghia@gmail.com>
2023-01-22 12:32:35 +0100

    rlang: simplify render.r

--- context/rlang/test/render.r
+++ context/rlang/test/render.r
@@ -1,4 +1,1 @@
-rmarkdown::render("test.rmd")
-output_dir <- "output"
-dir.create(output_dir, showWarnings = FALSE)
-file.copy("test.pdf", output_dir)
+rmarkdown::render("test.rmd", output_dir = "output")
